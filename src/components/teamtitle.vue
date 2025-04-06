f3a8cad40a6a0535abbfb891abf55b99f75de2ac
  Kanat Baibatyr <kanatbaibatyr@MacBook-Air-Kanat.local>
2025-04-06 13:54:01 +0500

    fix(team title page)

--- src/components/teamtitle.vue
+++ src/components/teamtitle.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="w-full flex justify-center absolute bottom-30 left-0 px-8 py-6">
-        <div class="w-4/5 flex flex-col items-start text-white gap-40 mt-20">
+    <div class="w-full flex justify-center absolute left-0 px-8 bottom-15">
+        <div class="w-4/5 flex flex-col items-start text-white gap-60 mt-20">
             <div class="flex gap-1">
                 <p>Главная /</p>
                 <div>
@@ -10,7 +10,7 @@
                 </div>
             </div>
             <div class="w-full flex justify-between">
-                <h1 class="text-7xl font-bold">Миссия школы</h1>
+                <h1 class="text-7xl font-bold">Наша команда</h1>
                 <div class="flex gap-2 items-center">
                     <img class="w-15 h-15" src="../assets/insta_white.svg" alt="">
                     <img class="w-15 h-15" src="../assets/whatsup_white.svg" alt="">
