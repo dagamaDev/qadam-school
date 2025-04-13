e8161fbc5c382d312034ae93f94770768ef499ea
  Kanat Baibatyr <kanatbaibatyr@MacBook-Air-Kanat.local>
2025-04-13 09:36:58 +0500

    feature(parents ppage and news carousel)

---
+++ src/components/parentsHero.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="w-full h-[70vh] flex justify-center w-3/4 bg-gradient-to-r from-[#eaac3f] to-[#eee03c]">
+        <div class="w-4/5 flex">
+            <div class="w-full flex flex-col justify-end text-white gap-40 mt-26 py-16">
+                <div class="flex gap-1">
+                    <p>Главная /</p>
+                    <div>
+                        <p class="border-b border-white">Миссия школы</p>
+                        <p>Наша команда</p>
+                        <p>Родителям</p>
+                    </div>
+                </div>
+                <div class="flex justify-between">
+                    <h1 class="text-7xl font-bold">Миссия школы</h1>
+                    <div class="flex gap-2 items-center">
+                        <img class="w-15 h-15" src="../assets/insta_white.svg" alt="">
+                        <img class="w-15 h-15" src="../assets/whatsup_white.svg" alt="">
+                        <img class="w-15 h-15" src="../assets/youtube_white.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/ParentsPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <parentsHero/>
+        <div class="mt-20">
+            <parentsGuidance/>
+        </div>
+        <div class="my-20">
+            <missionAlso />
+            <schoolNews />
+        </div>
+        <div class="bg-white">
+            <schoolQuestions class="py-20"/>
+        </div>
+        <div>
+            <schoolFooter/>
+        </div>
+    </div>
+</template>
+<script setup>
+import parentsHero from '../components/parentsHero.vue';
+import parentsGuidance from '../components/parentsGuidance.vue';
+import schoolQuestions from '../components/schoolQuestions.vue';
+import schoolFooter from '../components/schoolFooter.vue';
+import missionAlso from '../components/missionAlso.vue';
+import schoolNews from '../components/schoolNews.vue';
+</script>
