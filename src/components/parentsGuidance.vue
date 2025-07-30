bb93e8f14161e6d03d026c3f2c4b423164cba0af
  Kanat Baibatyr <kanatbaibatyr@MacBook-Air-Kanat.local>
2025-07-30 16:57:16 +0500

    feat(download file)

--- src/components/parentsGuidance.vue
+++ src/components/parentsGuidance.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="w-full flex justify-center">
-        <div class="w-3/4 flex flex-col md:flex-row gap-8">
+        <div class="md:w-3/4 mx-8 md:mx-0 flex flex-col md:flex-row gap-8">
             <div class="w-full md:w-3/7 flex flex-col gap-4">
                 <h1 class="text-4xl md:text-5xl font-bold">{{ $t("parents.title") }}</h1>
                 <div class="hidden md:flex flex-col gap-4">
-                    <button
+                    <button @click="downloadFile"
                         class="bg-gradient-to-r w-full from-orange-400 rounded-lg to-yellow-400 bg-white px-8 py-2">{{ $t("parents.download") }}</button>
                     <button @click="handleShowModal" class="border rounded-lg bg-white px-8 py-2">{{$t('main-btn')}}</button>
                 </div>
@@ -17,7 +17,7 @@
                 <p>{{ $t("parents.info.read") }}</p>
             </div>
             <div class="flex md:hidden flex-col gap-2">
-                    <button
+                    <button @click="downloadFile"
                         class="bg-gradient-to-r w-full from-orange-400 rounded-lg to-yellow-400 bg-white px-8 py-2">{{ $t("parents.download") }}</button>
                     <button @click="handleShowModal" class="cursor-pointer border rounded-lg bg-white px-8 py-2">{{$t('main-btn')}}</button>
                 </div>
@@ -30,5 +30,24 @@ const emit = defineEmits();
 const handleShowModal = () => {
     console.log('showModal')
     emit('showModal');
+};
+const downloadFile = () => {
+    console.log('download')
+    const lang = localStorage.getItem('lang');
+    if (lang === 'ru') {
+        const link = document.createElement('a');
+        link.href = 'https://drive.google.com/uc?export=download&id=1xvn_yFe50IBrB0w54IlJkavdGUC_UF0L';
+        link.download = 'document.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    } else if(lang === 'kz'){
+        const link = document.createElement('a');
+        link.href = 'https://drive.google.com/file/d/1NLrSOrIByAb-4JjmiR95GzPfeWrlDYn7/view?usp=drivesdk';
+        link.download = 'document.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
 };
 </script>
